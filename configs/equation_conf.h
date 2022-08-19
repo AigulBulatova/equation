4ecf3064c107feb9cf6eb0c3898ff3a6af0c53ca
AigulBulatova <aygul.bulatova.04@mail.ru>
2022-08-20 03:54:15 +0500

    .

--- configs/equation_conf.h
+++ configs/equation_conf.h
@@ -3,10 +3,12 @@
 ///This is a config file for testing and debugging an equation solver
 ///Below you can see short descriptions for configurations
 
-///Enables testing the program
-#define TESTING
+///Enables testing the program and prints results of testing
+//#define TESTING
 
 ///Enables debugging the program
+///
+///Prints coefficients of equation, discriminant and other information
 //#define DEBUG 
 
 #endif
